f519692883baee7280f9e5e67b20826e6c30d7ab
Maksim Uralskiy <36118971+SabishiSenshi@users.noreply.github.com>
2026-02-03 14:49:21 +0500

    Create Task_2.java

---
+++ HW_2/Task_2.java
@@ -0,0 +1,15 @@
+public class res {
+    public static void main(String[] args) {
+        // объявление переменной
+        int a = 35;
+        int b = 12;
+        // Ввод третьей временной переменной для хранения
+        int c = a;
+        // Присваиваем значения переменным
+        a = b;
+        b = c;
+        // Вывод
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
