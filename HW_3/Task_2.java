0a4e4b40fce75ec7f9f687c5dea16c8046c8f718
Maksim Uralskiy <36118971+SabishiSenshi@users.noreply.github.com>
2026-02-06 19:54:39 +0500

    Create Task_2.java

---
+++ HW_3/Task_2.java
@@ -0,0 +1,12 @@
+public class qwerty2 {
+    public static void main(String[] args) {
+        int c = 10;
+        int d = 12;
+
+        if ((c + d) % 2 == 0) {
+            System.out.println("maybe a and b are even");
+        } else {
+            System.out.println("some variable is odd");
+        }
+    }
+}
