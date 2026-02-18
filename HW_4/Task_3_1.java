0623c0e7c8a0553dabe5e72c6b70e50bec56171e
Maksim Uralskiy <36118971+SabishiSenshi@users.noreply.github.com>
2026-02-18 14:59:57 +0500

    Create Task_3_1.java

---
+++ HW_4/Task_3_1.java
@@ -0,0 +1,9 @@
+public class rez {
+    public static void main(String[] args) {
+        for (int a = 40; a <= 60; a++){
+            if (a % 4 == 0){
+                System.out.println(a);
+            }
+        }
+    }
+}
