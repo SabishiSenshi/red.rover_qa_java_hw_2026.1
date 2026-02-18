5ffc89155d8c9fc961603ce22a65349da2eb5fee
Maksim Uralskiy <36118971+SabishiSenshi@users.noreply.github.com>
2026-02-18 18:52:03 +0500

    Create Task_1.java

---
+++ HW_5/Task_1.java
@@ -0,0 +1,10 @@
+public class qwerty {
+    public static void main(String[] args) {
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+
+        for (int i = 0; i < array.length; i = i + 1){
+            if (array[i] % 2 != 0)
+                System.out.println(array[i]);
+        }
+    }
+}
