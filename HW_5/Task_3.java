a72b47937ae2aefb51681f8c130c55b40d29f091
Maksim Uralskiy <36118971+SabishiSenshi@users.noreply.github.com>
2026-02-18 18:54:35 +0500

    Create Task_3.java

---
+++ HW_5/Task_3.java
@@ -0,0 +1,9 @@
+public class qwerty {
+    public static void main(String[] args) {
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+
+        for (int i = 0; i < array.length; i = i + 1){
+            System.out.println(array[i] + 15);
+        }
+    }
+}
