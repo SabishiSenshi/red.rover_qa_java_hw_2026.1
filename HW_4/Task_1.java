ce8d4b76ac8abd120d8359a4a38be07d28faf7a6
Maksim Uralskiy <36118971+SabishiSenshi@users.noreply.github.com>
2026-02-18 15:27:33 +0500

    Update Task_1.java

--- HW_4/Task_1.java
+++ HW_4/Task_1.java
@@ -2,7 +2,7 @@ public class qwerty {
     public static void main(String[] args) {
         int a = 0;
 
-        while (a <= 15) {
+        while (a < 15) {
             System.out.println(a++);
         }
     }
