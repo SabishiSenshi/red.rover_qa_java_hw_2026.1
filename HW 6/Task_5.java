98bdd06c4b79c93ae722869f76401836ecf31b79
Maksim Uralskiy <36118971+SabishiSenshi@users.noreply.github.com>
2026-04-02 22:43:28 +0500

    Create Task_5.java

---
+++ HW 6/Task_5.java
@@ -0,0 +1,18 @@
+public class task {
+    public static void main(String[] args) {
+
+        int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+                int value = array[i][j];
+                if (value > max){
+                    max = value;
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
