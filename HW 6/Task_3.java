4cdbdd9f8420d20dcff028e6c14aa2b96f1f0b9e
Maksim Uralskiy <36118971+SabishiSenshi@users.noreply.github.com>
2026-04-02 22:27:49 +0500

    Create Task_3.java

---
+++ HW 6/Task_3.java
@@ -0,0 +1,12 @@
+public class task2 {
+    public static void main(String[] args) {
+
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        System.out.println(sum);
+    }
+}
